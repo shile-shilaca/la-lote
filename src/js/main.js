015b1a796a1580e61076f9daac17ed2494ade381
Hugo Habel <hugohabel@gmail.com>
2015-01-23 20:22:03 -0600

    Add 'Home' view

---
+++ src/js/main.js
@@ -0,0 +1,40 @@
+var app = angular.module('lottery', ['ngRoute']);
+
+/** Routes Configuration **/
+app.config(function ($routeProvider) {
+    /** Home **/
+    $routeProvider.when('/', {
+        templateUrl : './templates/home.tpl.html',
+        controller  : 'homeController'
+    })
+    /** Error View **/
+    .when('/error', {
+        templateUrl : './templates/error.tpl.html'
+    }).otherwise({
+        redirectTo  : '/'
+    });
+});
+
+app.run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function () {
+        $location.path('/error');
+    });
+    $rootScope.$on('$routeChangeStart', function () {
+        $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function () {
+        $rootScope.isLoading = false;
+    });
+});
+
+/** Define Controllers **/
+
+// Home Controller
+app.controller('homeController', ['$scope', '$location', '$anchorScroll',
+    function ($scope, $location, $anchorScroll) {
+        $scope.scrollTo = function (id) {
+            $location.hash(id);
+            $anchorScroll();
+        }
+    }
+]);
