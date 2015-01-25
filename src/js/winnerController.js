93fa3ca4f85973b9170b34522925e0d03d56e81a
huhgawz <antonio.aguilar@oracle.com>
2015-01-25 15:51:50 -0600

    Play Loteria audio

--- src/js/winnerController.js
+++ src/js/winnerController.js
@@ -1,5 +1,9 @@
 app.controller('winnerController', ['$scope', '$location',
     function($scope, $location) {
+        // Play "Loteria" audio
+        var audio = new Audio('audio/01.mp3');
+        audio.play();
+
         $scope.goTo = function(location) {
             $location.path('/' + location);
         };
